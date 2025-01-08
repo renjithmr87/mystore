41377f6c3e7da316e1f72e7613d94e17affea9af
renjumr87 <renjumr87@gmail.com>
2025-01-09 01:54:56 +0530

    production test

--- mystore/settings.py
+++ mystore/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback-secret-for-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'True') == 'False'
 
 
 # ALLOWED_HOSTS = ['mystore-vpex.onrender.com',]
